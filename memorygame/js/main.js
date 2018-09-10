ba9ab09f1be6a8486272a81d09c846a83bc2e0c8
Jack Bogart <bogart.jack98@gmail.com>
2018-09-10 04:59:34 -0600

    more changes yo

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,8 +1,18 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
-console.log("user flipped" + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length == 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("match");
+	}
+	else {
+		alert("you're dumb");
+	}
+
+}
