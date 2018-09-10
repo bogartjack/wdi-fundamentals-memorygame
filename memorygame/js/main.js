046e1d21f1b6bc525d4444cd72e6b7cb4d147f30
Jack Bogart <bogart.jack98@gmail.com>
2018-09-10 06:33:09 -0600

    finished

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,18 +1,56 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen", 
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen", 
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king", 
+		suit: "hearts", 
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king", 
+		suit: "diamonds", 
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
+var checkMatch = function(){
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length == 2){
 	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("match");
+		alert("You matched");
 	}
 	else {
-		alert("you're dumb");
+		alert("you're still dumb");
 	}
-
 }
+
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped" + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
+	checkMatch();
+}
+
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+} 
+
+createBoard();
